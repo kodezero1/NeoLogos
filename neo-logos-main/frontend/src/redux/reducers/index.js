e46727a375be078d3bb470d3735cd5582f3325cf
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-10-07 00:20:28 +0100

    Deleted neo-logos-main directory

--- neo-logos-main/frontend/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./userReducer";
-import wordsReducer from "../reducers/wordsReducer";
-
-const rootReducer = combineReducers({ user: userReducer, words: wordsReducer });
-
-export default rootReducer;
